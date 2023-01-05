f2a3d9aa6faf4cab20079e63eb5b46a514fa73e7
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2023-01-05 11:03:14 +0200

    Started form

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" , href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" , integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" , crossorigin="anonymous">
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
+    <link rel="stylesheet" href="src/styles.css">
+    <title>Apex SkateShop</title>
+
+</head>
+<body>
+    <div class="form-container"></div>
+
+      <form action='' method="post">
+        <h3>Register now</h3>
+        <input type="text" name="name" required placeholder="enter username" class="box">
+        <input type="email" name="email" required placeholder="enter email" class="box">
+        <input type="password" name="name" required placeholder="enter password" class="box">
+        <input type="cpassword" name="name" required placeholder="confirm username" class="box">
+        <input type="submit" name="submit" class="btn" value="register now">
+        <p>Already have an account? <a href="login.php">Login now</a></p>
+      </form>
+</body>
+</html>
