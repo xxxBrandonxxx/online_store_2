7440d849ede9ab2594d9130c271a1c89bf5b75e6
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2023-01-06 14:27:26 +0200

    grand total working

--- index.php
+++ index.php
@@ -18,25 +18,24 @@ if (isset($_GET['logout'])) {
     header('location:login.php');
 };
 
-if(isset($_POST['add_to_cart'])){
+if (isset($_POST['add_to_cart'])) {
 
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-   $product_image = $_POST['product_image'];
-   $product_quantity = $_POST['product_quantity'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
 
-   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name'
-    AND user_id = '$user_id'") or die ('query failed');
+    $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name'
+    AND user_id = '$user_id'") or die('query failed');
 
-   if(mysqli_num_rows($select_cart) > 0){
-    $message[] = 'product already added to cart!';
-   }else{
-    mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, image, quantity) VALUES
+    if (mysqli_num_rows($select_cart) > 0) {
+        $message[] = 'product already added to cart!';
+    } else {
+        mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, image, quantity) VALUES
     ('$user_id', '$product_name', '$product_price', '$product_image', '$product_quantity')")
-     or die('query failed');
-     $message[] = 'product added to cart!';
-   }
-
+            or die('query failed');
+        $message[] = 'product added to cart!';
+    }
 }
 
 
@@ -83,15 +82,13 @@ if(isset($_POST['add_to_cart'])){
             <p> username : <span><?php echo $fetch_user['name']  ?></span> </p>
             <p> email : <span><?php echo $fetch_user['email'] ?></span> </p>
             <div class="flex">
-                <!-- <a href="login.php" class="btn btn-outline-light btn-lg px-5">Login</a>
-            <a href="register.php" class="btn btn-outline-light btn-lg px-5">Register</a> -->
                 <a href="index.php?logout=<?php echo $user_id; ?>" onclick="return confirm('Are you sure you want to Logout?');" class="btn btn-warning">Logout</a>
             </div>
         </div>
 
         <div class="products">
 
-          <h1 class="heading">Latest products</h1>
+            <h1 class="heading">Latest products</h1>
 
             <div class="box-container">
 
@@ -108,7 +105,7 @@ if(isset($_POST['add_to_cart'])){
                             <input type="number" min="1" name="product_quantity" value="1">
                             <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?> ">
                             <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
-                            <input type="hidden" name="product_price"  value="<?php echo $fetch_product['price']; ?>">
+                            <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
                             <input type="submit" value="add to cart" name="add_to_cart" class="btn btn-primary">
 
                         </form>
@@ -124,10 +121,58 @@ if(isset($_POST['add_to_cart'])){
 
         </div>
 
+        <div class="shopping-cart">
+            <h1 class="heading">shopping cart</h1>
+
+            <table>
+                <thead>
+                    <th>image</th>
+                    <th>name</th>
+                    <th>price</th>
+                    <th>quantity</th>
+                    <th>total price</th>
+                    <th>action</th>
+                </thead>
+                <tbody>
+                    <?php
+                    $grand_total = 0;
+                    $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query_failed');
+                    if (mysqli_num_rows($cart_query) > 0) {
+                        while ($fetch_cart = mysqli_fetch_assoc($cart_query)) {
+                        $sub_total = $fetch_cart['price'] * $fetch_cart['quantity'];
+                    ?>
+                        <tr>
+                            <td><img src="src/images/<?php echo $fetch_cart['image']; ?>" height="100" alt="images of skateboards"></td>
+                            <td><?php echo $fetch_cart['name']; ?></td>
+                            <td>R <?php echo $fetch_cart['price']; ?></td>
+                            <td>
+                                <form action="" method="post">
+                                    <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>">
+                                    <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>">
+                                    <input type="submit" name="update_cart" value="update" class="btn btn-secondary">
+                                </form>
+                            </td>
+                            <td>R <?php echo number_format($sub_total); ?></td>
+                            <td><a href="index.php?remove=<?php echo $fetch_cart['id']; ?>" class="btn btn-danger" onclick="return confirm('remove item from cart?');">remove</a></td>
+                        </tr>
+
+                    <?php
+                            $grand_total += $sub_total;
+                        };
+                    };
+                    ?>
+                    <tr class="table-bottom">
+                        <td colspan="4">grand total :</td>
+                        <td>R<?php echo $grand_total; ?></td>
+                        <td><a href="index.php?delete_all" class="btn btn-danger" onclick="return confirm('delete all from cart?');">delete all</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
     </div>
 
 
 </body>
 
-</html>
+</html>;
