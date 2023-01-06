514248f8c9cc5388685cc5282f034384b8d07fe3
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2023-01-06 10:37:43 +0200

    add to cart working with database

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include 'config.php';
 session_start();
 $user_id = $_SESSION['user_id'];
@@ -7,11 +11,36 @@ $user_id = $_SESSION['user_id'];
 if (!isset($user_id)) {
     header('location:login.php');
 };
+
 if (isset($_GET['logout'])) {
     unset($user_id);
     session_destroy();
     header('location:login.php');
+};
+
+if(isset($_POST['add_to_cart'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = $_POST['product_quantity'];
+
+   $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name'
+    AND user_id = '$user_id'") or die ('query failed');
+
+   if(mysqli_num_rows($select_cart) > 0){
+    $message[] = 'product already added to cart!';
+   }else{
+    mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, image, quantity) VALUES
+    ('$user_id', '$product_name', '$product_price', '$product_image', '$product_quantity')")
+     or die('query failed');
+     $message[] = 'product added to cart!';
+   }
+
 }
+
+
+
 
 ?>
 
@@ -59,6 +88,43 @@ if (isset($_GET['logout'])) {
                 <a href="index.php?logout=<?php echo $user_id; ?>" onclick="return confirm('Are you sure you want to Logout?');" class="btn btn-warning">Logout</a>
             </div>
         </div>
+
+        <div class="products">
+
+          <h1 class="heading">Latest products</h1>
+
+            <div class="box-container">
+
+                <?php
+                $select_product = mysqli_query($conn, "SELECT * FROM `products`") or die('query_failed');
+                if (mysqli_num_rows($select_product) > 0) {
+                    while ($fetch_product = mysqli_fetch_assoc($select_product)) {
+                ?>
+
+                        <form method="post" class="box" action="">
+                            <img src="src/images/<?php echo $fetch_product['image']; ?>" alt="skateboard image" id="images">
+                            <div class="name"><?php echo $fetch_product['name']; ?></div>
+                            <div class="price">R<?php echo $fetch_product['price']; ?>.00</div>
+                            <input type="number" min="1" name="product_quantity" value="1">
+                            <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?> ">
+                            <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
+                            <input type="hidden" name="product_price"  value="<?php echo $fetch_product['price']; ?>">
+                            <input type="submit" value="add to cart" name="add_to_cart" class="btn btn-primary">
+
+                        </form>
+
+                <?php
+                    }
+                };
+
+                ?>
+
+
+            </div>
+
+        </div>
+
+
     </div>
 
 
